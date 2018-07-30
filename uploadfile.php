7ae142bc307040ad51cfc93a6bdd58a2309e1e13
Vicky Daiya <vicky.daiya@somaiya.edu>
2018-07-30 21:25:14 +0530

    updated 30/7/18

--- uploadfile.php
+++ uploadfile.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ERROR | E_PARSE);
 date_default_timezone_set("Asia/Calcutta");
 $target_dir = "uploads/";
 $filenames = array();
@@ -6,72 +7,118 @@ $targetfiles = array();
 $uploadstatus=array();
 $filetypes=array();
 $datetime=array();
-for ($x=0; $x <8 ; $x++) { 
-$filenames[$x]=$_FILES['fileuploaded'.($x+1)]['name']; 
-$targetfiles[$x]= $target_dir . basename($_FILES['fileuploaded'.($x+1)]['name']); 
-$uploadstatus[$x]=1;
-$filetypes[$x]=strtolower(pathinfo($targetfiles[($x)],PATHINFO_EXTENSION));
+$semselected =$_POST['sem_upload'];
+$acyearselected = $_POST['academic_year_upload']; 
+for ($x=0; $x <8 ; $x++) 
+{ 
+    $filenames[$x]=$_FILES['fileuploaded'.($x+1)]['name']; 
+    $targetfiles[$x]= $target_dir . basename($_FILES['fileuploaded'.($x+1)]['name']); 
+    $uploadstatus[$x]=1;
+    $filetypes[$x]=strtolower(pathinfo($targetfiles[($x)],PATHINFO_EXTENSION));
 }
+
 if(isset($_POST['submitfileup']))
-for ($x=0; $x <8 ; $x++)
 {
-    if($filetypes[$x]!='csv')
+    for ($x=0; $x <8 ; $x++)
     {
-        $uploadstatus[$x]=0;
+        if($filetypes[$x]!='csv')
+        {
+            $uploadstatus[$x]=0;
+        }
+        else
+        {
+            $uploadstatus[$x]=1;
+        }
+
     }
-    else
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $db='analyzerdb';
+    $conn = mysqli_connect($servername,$username,$password,$db);
+    
+    if (!$conn) 
+        {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+    for($x=0;$x<8;$x++)
     {
-        $uploadstatus[$x]=1;
+        if(file_exists($targetfiles[$x]))
+        {
+            $tablename =  basename($targetfiles[$x],".csv");
+            $sql = "DROP TABLE ".$tablename;
+            mysqli_query($conn,$sql);
+            $sql = "DELETE FROM FILES_UPLOADED WHERE filename = '".$_FILES['fileuploaded'.($x+1)]['name']."'";
+            mysqli_query($conn,$sql);
+        }
     }
 
-}
-for ($x=0; $x <8 ; $x++)
-{
-   if (file_exists($targetfiles[$x])) {
-    $uploadstatus[$x] = 0;
-}
-}
-
-for ($x=0; $x <8 ; $x++)
-{
-   if($uploadstatus[$x]==1) {
-    if (move_uploaded_file($_FILES['fileuploaded'.($x+1)]['tmp_name'], $targetfiles[$x])) {
-        //echo "The file ". basename( $_FILES['fefileuploaded']['name']). " has been uploaded.";
-        $datetime[$x]=date("Y/m/d h:i:sa");
-    } else {
-        //echo "Sorry, there was an error uploading your file.";
-    }
-
-}
-}
+   
 
 
+    for ($x=0; $x <8 ; $x++)
+        {
+            if($uploadstatus[$x]==1) 
+                {
+                    if (move_uploaded_file($_FILES['fileuploaded'.($x+1)]['tmp_name'], $targetfiles[$x])) 
+                        {
+                            $tablename =  basename($targetfiles[$x],".csv");
+                            $datetime[$x]=date("Y/m/d h:i:sa");
+                            $file = fopen($targetfiles[$x], "r");
+                            $create = "CREATE TABLE ".$tablename." (";
+                            $sql="";
+                            $attr = fgetcsv($file);
+                            for($z=0;$z<count($attr);$z++)
+                                {
+                                    $sql =$sql."$attr[$z] varchar(255),";
+                                }
+                            $newsql=rtrim($sql,", ");
+                            $sql = $create.$newsql.")"; 
+                            if(mysqli_query($conn,$sql))
+                                {
+                                    
+                                }
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$db='analyzer project';
-$conn = mysqli_connect($servername,$username,$password,$db);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+                            while(!feof($file))
+                                {
+                                    $sql="";
+                                    $ys = "INSERT INTO " .$tablename." VALUES (";
+                                    $tuples = fgetcsv($file);
+                                    for($y=0;$y<count($tuples);$y++)
+                                        {
+                                            $sql =$sql."'$tuples[$y]',";
+                                        }
+                                    $newsql=rtrim($sql,", ");
+                                    $sql = $ys.$newsql.")";
+                                    if(mysqli_query($conn,$sql))
+                                        {
+                                            
+                                        }
+                                }
+                            fclose($file);    
+                        }
+                }
+        }                    
 
-for($x=0;$x<8;$x++)
-{
-    if($uploadstatus[$x]==1){
-        $sql = "INSERT INTO FILES_UPLOADED VALUES ('$filenames[$x]','$datetime[$x]');";
+
+    for($x=0;$x<8;$x++)
+        {
+            if($uploadstatus[$x]==1)   
+                {
+                    $sql = "INSERT INTO FILES_UPLOADED VALUES ('$filenames[$x]','$datetime[$x]');";
  
-if(mysqli_query($conn,$sql))
-{
-    echo 'yess';
-}   
-else
-{
-    echo 'fuck';
-    }
-}
-}
+                    if(mysqli_query($conn,$sql))
+                        {
+                           
+                        } 
+                }
+        }
 
+        
+}
 mysqli_close($conn);
-
+header("Location: admin.php");
 ?>
+
